1837eda7144e31ba968d93c837b072da858c7033
Qiu RenBo <lampqiu@sina.com>
2020-06-17 10:29:03 +0800

    fix: fix dir name

---
+++ src/models/handout/purchase.model.ts
@@ -0,0 +1,62 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../../config/mariadb";
+export default class Purchase extends Model {
+  id: string; // ⇨ uuid'9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+  date: string;
+  projectName: string;
+  productName: string;
+  unitPrice: number;
+  number: number;
+  totalPrice: number;
+  source: string;
+  remarks: string;
+}
+
+Purchase.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    date: {
+      type: DataTypes.STRING,
+    },
+    projectName: {
+      type: DataTypes.STRING,
+    },
+    productName: {
+      type: DataTypes.STRING,
+    },
+    unitPrice: {
+      type: DataTypes.FLOAT,
+    },
+    number: {
+      type: DataTypes.INTEGER,
+    },
+    totalPrice: {
+      type: DataTypes.FLOAT,
+    },
+    source: {
+      type: DataTypes.STRING,
+    },
+    remarks: {
+      type: DataTypes.TEXT,
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  },
+
+  {
+    sequelize,
+    tableName: "purchases",
+  }
+);
+// Purchase.sync({ force: true });
